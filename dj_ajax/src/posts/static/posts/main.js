ea91e02bacf75712138ffaa83a0ea07ed4ad3376
ygmzsz <ygnaciomzsz@gmail.com>
2025-04-10 19:04:09 -0400

    added better ajax handling according to getting posts objects with ajax

--- dj_ajax/src/posts/static/posts/main.js
+++ dj_ajax/src/posts/static/posts/main.js
@@ -1,16 +1,34 @@
 console.log("hello world!")
 
 const helloWorldBox = document.getElementById('hello-world')
+const postBox = document.getElementById('posts-box')
 
 $.ajax({
-    typeo: 'Get',
+    type: 'GET', 
     url: '/hello-world/',
-    success: function(response)
-    {
+    success: function(response) {
         console.log('success', response)
+        helloWorldBox.innerHTML = response.text 
     },
-    erro: function(error)
-    {
+    error: function(error) {  
+        console.log('error:', error)
+    }
+})
+
+$.ajax({
+    type: 'GET',
+    url: '/data/',
+    success: function(response) {
+        console.log('success', response)
+        const data = response.data  
+        console.log(data)
+        data.forEach(element => {
+            postBox.innerHTML += `
+            ${element.title} - <b>${element.body}</b><br>
+            `
+        });
+    },
+    error: function(error) { 
         console.log('error:', error)
     }
 })
--- dj_ajax/src/posts/urls.py
+++ dj_ajax/src/posts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import (
     post_list_and_create,
+    load_post_data_view,
 
     hello_world_view
 )
@@ -9,6 +10,7 @@ app_name = 'posts'
 
 urlpatterns = [
     path('', post_list_and_create, name='main-board'),
+    path('data/', load_post_data_view, name='posts-data'),
 
     path('hello-world/', hello_world_view, name='hello-world')
 ]
