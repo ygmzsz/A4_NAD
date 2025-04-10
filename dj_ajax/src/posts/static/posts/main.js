be60fd7540fc39dc741964bde1ded7a578416b7a
ygmzsz <ygnaciomzsz@gmail.com>
2025-04-10 19:49:28 -0400

    added a the post cards, and all the Ui from the creating post cards video

--- dj_ajax/src/posts/static/posts/main.js
+++ dj_ajax/src/posts/static/posts/main.js
@@ -2,6 +2,7 @@ console.log("hello world!")
 
 const helloWorldBox = document.getElementById('hello-world')
 const postBox = document.getElementById('posts-box')
+const spinnerBox = document.getElementById('spinner-box')
 
 $.ajax({
     type: 'GET', 
@@ -21,12 +22,31 @@ $.ajax({
     success: function(response) {
         console.log('success', response)
         const data = response.data  
+       setTimeout(()=>{
+        spinnerBox.classList.add('not-visible')
         console.log(data)
         data.forEach(element => {
             postBox.innerHTML += `
-            ${element.title} - <b>${element.body}</b><br>
+            <div class="card mb-2">
+                <div class="card-body">
+                    <h5 class="card-title">${element.title}</h5>
+                    <p class="card-text">${element.body}</p>
+                </div>
+                <div class="card-footer">
+                    <div class="row">
+                        <div class="col-1">
+                            <a href="#" class="btn btn-primary">Details</a>
+                        </div>
+                        <div class="col-1">
+                            <a href="#" class="btn btn-primary">like</a>
+                        </div>
+                    </div>        
+                </div>
+            </div> 
             `
         });
+       }
+    )
     },
     error: function(error) { 
         console.log('error:', error)
