30eccc3a6c209972291115d3a6acb4f68962c7bf
ygmzsz <ygnaciomzsz@gmail.com>
2025-04-11 11:47:40 -0400

    made a working details page, and button linking

--- dj_ajax/src/posts/static/posts/main.js
+++ dj_ajax/src/posts/static/posts/main.js
@@ -11,6 +11,8 @@ const body = document.getElementById('id_body')
 const csrf = document.getElementsByName('csrfmiddlewaretoken')
 console.log('csrf', csrf)
 
+const url = window.location.href
+
 const alertBox = document.getElementById('alert-box')
 
 const getCookie =(name) =>
@@ -79,7 +81,7 @@ const getData = () => {
                     <div class="card-footer">
                         <div class="row">
                             <div class="col-2">
-                                <a href="#" class="btn btn-primary">Details</a>
+                                <a href="${url}${element.id}" class="btn btn-primary">Details</a>
                             </div>
                             <div class="col-2"> 
                                 <form class="like-unlike-forms" data-form-id="${element.id}">
--- dj_ajax/src/posts/urls.py
+++ dj_ajax/src/posts/urls.py
@@ -3,6 +3,7 @@ from .views import (
     post_list_and_create,
     load_post_data_view,
     like_unlike_post,
+    post_detail,
 
     hello_world_view
 )
@@ -12,8 +13,9 @@ app_name = 'posts'
 urlpatterns = [
     path('', post_list_and_create, name='main-board'),
     path('like-unlike/', like_unlike_post, name='like-unlike'),
+    path('<pk>/', post_detail, name='post-detail'),
     path('data/<int:num_posts>/', load_post_data_view, name='posts-data'),
-
-    path('hello-world/', hello_world_view, name='hello-world')
+    
+    
 ]
 
